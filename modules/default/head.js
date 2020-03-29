9f7c66793c2ff1c482fe761e7110279863e542f1
LPlox <lina.js.wang@gmail.com>
2020-03-29 19:58:21 +0200

    trying to deploy

--- modules/default/head.js
+++ modules/default/head.js
@@ -9,11 +9,7 @@ const Head = ({ title, description }) => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" href="/favicon.ico" />
     <link
-      rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Zilla+Slab:400,700"
-    />
-    <link
-      href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap"
+      href="https://fonts.googleapis.com/css?family=Playfair+Display:500,700&display=swap"
       rel="stylesheet"
     ></link>
   </NextHead>
