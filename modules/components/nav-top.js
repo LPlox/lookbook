2e5a11cf5edc0c7bf6eef60ae230c8d643b9fc3e
LPlox <lina.js.wang@gmail.com>
2020-04-01 21:22:57 +0200

    gonna probably stop commiting to master so nothing fucks up

--- modules/components/nav-top.js
+++ modules/components/nav-top.js
@@ -1,12 +1,11 @@
 import Link from "next/link";
-
 import "../scss/load-presets.scss";
 import "../scss/grid4x4.scss";
 import "../scss/nav-allcollections.scss";
 
 const Nav = () => {
   return (
-    <nav className="flex-container mobile-deactive" id="allcollection-nav ">
+    <nav className="flex-container mobile-deactive" id="allcollection-nav">
       <Link href="/">
         <a className="link-font flex-item text-center">All</a>
       </Link>
